b9c9c196507bdeda3c2be942d1a41665f870dfd2
adriansyah <adriansyah.azhari@kelaspintar.id>
2023-08-23 16:29:04 +0700

    add confirm payment

--- apps/api_cofee_app/src/modules/user/user.controller.ts
+++ apps/api_cofee_app/src/modules/user/user.controller.ts
@@ -1,10 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ParseIntPipe } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, ParseIntPipe, UploadedFile, ParseFilePipeBuilder, HttpStatus, UseInterceptors } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { JwtAuthGuard } from '@app/jwt/jwt_guard/jwt_guard.guard';
 import { RoleGuardGuard } from '../../role-guard/role-guard.guard';
 import { Roles } from 'libs/custom_decorators/roles.decorators';
+import { Express } from 'express';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('api/v1/user')
 export class UserController {
@@ -47,15 +49,23 @@ export class UserController {
 
   @UseGuards(JwtAuthGuard,RoleGuardGuard)
   @Roles(2)
+  @UseInterceptors(FileInterceptor('file'))
   @Patch('transaction/confirm/:id')
-  async confirmTransaction(@Param('id', ParseIntPipe) id : number ) {
-    return await this.userService.confirmTransaction();
+  async confirmTransaction(@Param('id', ParseIntPipe) id : number,@UploadedFile(
+    new ParseFilePipeBuilder()
+      .addFileTypeValidator({
+      fileType: new RegExp(/[^\s]+(.*?).(jpg|jpeg|png|JPG|JPEG|PNG)$/),
+    })
+    .addMaxSizeValidator({
+      maxSize: 1000000 * 10
+    })
+    .build({
+      fileIsRequired: false,
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY
+    }),
+  ) file : Express.Multer.File ) {
+    return await this.userService.confirmTransaction(id,file);
   }
-
-
-
-
-
   // async UpdateProduct(@Param('id', ParseIntPipe) id : number, @Body(new TransformIntPipe()) productupdatedto : ProductUpdateDto, @UploadedFile(
   //   new ParseFilePipeBuilder()
   //   .addFileTypeValidator({
---
+++ prisma/migrations/20230823084922_add_image_in_transaction/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "User_Transaction" ADD COLUMN     "confirm_payment_image" TEXT;
