294d757fb2d75486c7af364c87de945e0b822c59
adriansyah <adriansyah.azhari@kelaspintar.id>
2023-08-23 14:43:06 +0700

    add list user and list deleted user

--- apps/api_cofee_app/src/modules/modules.module.ts
+++ apps/api_cofee_app/src/modules/modules.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { ProductModule } from './product/product.module';
 import { TransactionModule } from './transaction/transaction.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, ProductModule, TransactionModule]
+  imports: [AuthModule, ProductModule, TransactionModule, UserModule]
 })
 export class ModulesModule {}
---
+++ apps/api_cofee_app/src/modules/user/user.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { UtilsModule } from '@app/utils';
+
+@Module({
+  imports: [UtilsModule],
+  controllers: [UserController],
+  providers: [UserService]
+})
+export class UserModule {}
